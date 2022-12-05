888c9e98b8fc93abd7b4c6ed0f502eb07c31669c
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2022-12-05 17:49:51 +0530

    Add declarations

---
+++ modules/declaration.js
@@ -0,0 +1,17 @@
+// Inputs
+export const title = document.querySelector('#title');
+export const author = document.querySelector('#author');
+
+// Buttons
+export const addBtn = document.querySelector('#add');
+export const listBtn = document.querySelector('#listBtn');
+export const adBtn = document.querySelector('#addBtn');
+export const conBtn = document.querySelector('#conBtn');
+
+// Sections
+export const listSec = document.querySelector('#listSec');
+export const addSec = document.querySelector('#addSec');
+export const conSec = document.querySelector('#conSec');
+
+// Time
+export const timeDiv = document.querySelector('#time');
