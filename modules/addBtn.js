6c7521bafc1ef8dfd1e49c7b188a89981e04f6dc
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2022-12-05 18:53:16 +0530

    Fix Linter errors

--- modules/addBtn.js
+++ modules/addBtn.js
@@ -1,16 +1,18 @@
 import { BooksClass } from './class.js';
-import { title, author, req, suc } from './declaration.js';
+import {
+  title, author, req, suc,
+} from './declaration.js';
 
 const addBtnFunc = () => {
     if (title.value == "" || author.value == "") {//eslint-disable-line
-     req.style.display = "flex";
+    req.style.display = 'flex';
   } else {
     const bookAd = new BooksClass();
     bookAd.add();
-    req.style.display = "none";
-    suc.style.display = "flex";
-    title.value = "";
-    author.value = "";
+    req.style.display = 'none';
+    suc.style.display = 'flex';
+    title.value = '';
+    author.value = '';
   }
 };
 
--- modules/class.js
+++ modules/class.js
@@ -8,45 +8,45 @@ export class BooksClass {
   }
 
     add = () => { //eslint-disable-line
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const book = {
-      Title: title,
-      author,
-    };
+      const title = document.querySelector('#title').value;
+      const author = document.querySelector('#author').value;
+      const book = {
+        Title: title,
+        author,
+      };
 
-    const netBooks = JSON.parse(localStorage.getItem('books'));
-    if (!netBooks) {
-      booksData.push(book);
-      localStorage.setItem('books', JSON.stringify(booksData));
+      const netBooks = JSON.parse(localStorage.getItem('books'));
+      if (!netBooks) {
+        booksData.push(book);
+        localStorage.setItem('books', JSON.stringify(booksData));
             location.reload();//eslint-disable-line
-    } else {
-      netBooks.push(book);
-      localStorage.setItem('books', JSON.stringify(netBooks));
+      } else {
+        netBooks.push(book);
+        localStorage.setItem('books', JSON.stringify(netBooks));
             location.reload(); //eslint-disable-line
+      }
     }
-  }
 
     showBooks = () => { //eslint-disable-line
-    const divP = document.querySelector('#booksIn');
-    const getBooks = JSON.parse(localStorage.getItem('books'));
+      const divP = document.querySelector('#booksIn');
+      const getBooks = JSON.parse(localStorage.getItem('books'));
 
-    if (!getBooks || !getBooks[0]) {
-      divP.innerHTML = '<div class="no-cards">No Books to Show...</div>';
-    } else {
-      for (let i = 0; i < getBooks.length; i += 1) {
-        const temp = i;
-        divP.innerHTML += `
+      if (!getBooks || !getBooks[0]) {
+        divP.innerHTML = '<div class="no-cards">No Books to Show...</div>';
+      } else {
+        for (let i = 0; i < getBooks.length; i += 1) {
+          const temp = i;
+          divP.innerHTML += `
               <div class="cards" data-id="${temp}">
                   <p>"${getBooks[i].Title}" by ${getBooks[i].author}</p><button class="remove" data-id='${temp}'>Remove</button>
               </div>
               `;
+        }
       }
     }
-  }
 
     del = (id) => { //eslint-disable-line
-    getBooksag.splice(id, 1);
-    localStorage.setItem('books', JSON.stringify(getBooksag));
-  }
+      getBooksag.splice(id, 1);
+      localStorage.setItem('books', JSON.stringify(getBooksag));
+    }
 }
