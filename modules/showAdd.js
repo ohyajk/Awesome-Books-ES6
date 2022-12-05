f4d7f0bf911b7fe5bc4ea2aacb944a25fa1f827e
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2022-12-05 17:51:28 +0530

    Add input section

---
+++ modules/showAdd.js
@@ -0,0 +1,9 @@
+import { listSec, addSec, conSec } from './declaration.js';
+
+const saf = () => {
+  listSec.style.display = 'none';
+  addSec.style.display = 'flex';
+  conSec.style.display = 'none';
+};
+
+export default saf;
